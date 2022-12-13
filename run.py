4fd7dbffa349f033e60a98b42056998984c98147
kaltun-dev <kaltun19932014@gmail.com>
2022-12-13 23:39:23 +0000

    add game line and board

--- run.py
+++ run.py
@@ -1,22 +1,26 @@
-# game logic 
 
 # introduction to game
+# set game
+board = [ "-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+CurrentPlayer = "x"
+winner = None
+gameRunning = True
 
 # print game board
 
-def game-board = [
-    "-" "-" "-"
-    "-" "-" "-"
-    "-" "-" "-"
-]
 
-# add game lines
-# set players
-# start playing 
-# choose grid number
-# check if taken
-# computer turn
-# check winner
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
 
-
+# take player input
+# check for win or tie
+# switch player
+# check for win or tie again
+# swich the player 
+# check for win or tie again.
 
