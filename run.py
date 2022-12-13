a496baf085903f7c484aefd349fbe7048b825f42
kaltun-dev <kaltun19932014@gmail.com>
2022-12-13 21:56:02 +0000

    add game logic follow

--- run.py
+++ run.py
@@ -1,2 +1,22 @@
-# 
+# game logic 
+
+# introduction to game
+
+# print game board
+
+def game-board = [
+    "-" "-" "-"
+    "-" "-" "-"
+    "-" "-" "-"
+]
+
+# add game lines
+# set players
+# start playing 
+# choose grid number
+# check if taken
+# computer turn
+# check winner
+
+
 
