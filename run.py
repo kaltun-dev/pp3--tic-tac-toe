8dcc072d19831c44172fff1d81d06308b2ef3d6a
kaltun-dev <kaltun19932014@gmail.com>
2022-12-14 12:48:14 +0000

    add welccome message

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-
-# introduction to game
 # set game
 board = [ "-", "-", "-",
          "-", "-", "-",
@@ -9,13 +7,18 @@ CurrentPlayer = "x"
 winner = None
 gameRunning = True
 
+# introduction to game
+name = input("Enter your name: ")
+print( 'WELLCOME to tic tac toe ' + name + ' place X on board') 
+print( 'To win, match 3 slots vertically horizentally or diagnally. ')
+
 # print game board
+def printBoard():
+     print(board[0] + " | " + board[1] + " | " + board[2])
+     print(board[3] + " | " + board[4] + " | " + board[5])
+     print(board[6] + " | " + board[7] + " | " + board[8])
 
-
-def printBoard(board):
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print(board[6] + " | " + board[7] + " | " + board[8])
+printBoard()
 
 # take player input
 # check for win or tie
