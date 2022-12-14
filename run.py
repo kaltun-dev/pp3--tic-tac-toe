86d5dbbd284d579c2c0e97844514910f42837313
kaltun-dev <kaltun19932014@gmail.com>
2022-12-14 23:59:24 +0000

    add game wining conditions

--- run.py
+++ run.py
@@ -34,6 +34,40 @@ def playerInput(board):
 
 
 # check for win or tie
+def checkHorizentalRow(board):
+     global winner 
+     if board[0] == board[1] == board[2] and board[1] != "-":
+          winner = board[0]
+          return True
+     elif board[3] == board[4] == board[5] and board[3] != "-":
+          winner = board[3]
+          return True
+     elif board[6] == board[7] == board[8] and board[6] != "-":
+          winner = board[6]
+          return True
+
+def checkVerticalRow(board):
+     if board[0] == board[3] == board[6] and board[0] != "-":
+          winner = board[0]
+          return True
+     elif board[1] == board[4] == board[7] and board[1] != "-":
+          winner = board[1]
+          return True
+     elif board[2] == board[5] == board[8] and board[2] != "-":
+          winner = board[2]
+          return True
+
+def checkDiagnalRow(board):
+     if board[0] == board[4] == board[8] and board[0] != "-":
+          winner = board[0]
+          return True
+     elif board[2] == board[4] == board[6] and board[2] != "-":
+          winner = board[2]
+          return True
+
+# check for win or tie
+
+
 # switch player
 # check for win or tie again
 # swich the player 
