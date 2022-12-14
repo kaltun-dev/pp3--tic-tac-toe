08599e84bc470151cdc4f8a1ff9ab03a7fd207a4
kaltun-dev <kaltun19932014@gmail.com>
2022-12-14 16:29:41 +0000

    add player to choose a spot on board

--- run.py
+++ run.py
@@ -15,16 +15,29 @@ print( 'To win, match 3 slots vertically horizentally or diagnally. ')
 # print game board
 def printBoard():
      print(board[0] + " | " + board[1] + " | " + board[2])
+     print("____________")
      print(board[3] + " | " + board[4] + " | " + board[5])
+     print("____________")
      print(board[6] + " | " + board[7] + " | " + board[8])
 
 printBoard()
 
 # take player input
+def CurrentPlayerChooses(board):
+     xInput = int(input("please enter a number from 1 - 9: "))
+     if xInput >= 1 and xInput <= 9 and board[xinput-1] == "-":
+          board[xInput-1] = CurrentPlayer
+     else: 
+          print("sorry, spot is occupied.") 
+
+
+CurrentPlayerChooses(board)
+
 
 # check for win or tie
 # switch player
 # check for win or tie again
 # swich the player 
 # check for win or tie again.
-
+# startgame function 
+# clear game function
