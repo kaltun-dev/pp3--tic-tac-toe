665b0f26d009862ca176abfd9d3fb2e270013edb
kaltun-dev <kaltun19932014@gmail.com>
2022-12-16 14:18:36 +0000

    refactor: removed all trial code and commented out.

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import random
 # set game
 board = ["-", "-", "-",
-            "-", "-", "-",
-            "-", "-", "-"]
+         "-", "-", "-",
+         "-", "-", "-"]
 
 currentPlayer = "x"
 winner = None
@@ -19,18 +19,6 @@ def getUsername():
             print( 'To win, match 3 slots vertically horizentally or diagnally. ')
             print("-----------------------------------------------")
             break
-"""
-# introduction to game
-name = input("Enter your name: ")
-if not name:
-    print("Name cannot be empty! Try again")
-    gameRunning = False
-    name = input("Enter your name: ")
-
-print( 'WELLCOME to tic tac toe ' + name + ' place X on board') 
-print( 'To win, match 3 slots vertically horizentally or diagnally. ')
-print("-----------------------------------------------")
-"""
 
 # print game board
 def printBoard():
@@ -40,23 +28,22 @@ def printBoard():
     print("__________")
     print(board[6] + " | " + board[7] + " | " + board[8])
 
+def playerInput():
+    """Get player choice."""
 
-# take player input
-def playerInput(board):
-    inp = int(input("Select a spot 1-9: "))
-    if not type(inp) is int:
-        raise TypeError("Only integers are allowed")
-    elif inp < 0 or inp > 9:
-        raise Exception("Sorry, no numbers below zero or above 9")
+    while True:
         inp = int(input("Select a spot 1-9: "))
-    # elif inp != int:
-        #print("Oops player is already at that spot.")
-    else:
-        board[inp-1] == "-"
-        board[inp-1] = currentPlayer
+        if inp < 0 or inp > 9:
+            print('Not a valid choice, please enter another value between 1 and 9.')
+        elif board[inp-1] == int:
+            print("Sorry that position is taken")
+        else:
+            board[inp-1] == "-"
+            board[inp-1] = currentPlayer
+            break
 
 
-# check for win or on diffret positions
+# check for win or on different positions
 def checkHorizontleRow(board):
     global winner
     if board[0] == board[1] == board[2] and board[0] != "-":
@@ -118,51 +105,29 @@ def checkwinner():
         printBoard()
         askUser = input("Thank you for playing !!, would you like to play again?, press y for yes, n for no. ")
         if askUser == "y":
-            playerInput.clear()
-        #gameRunning = False
+            main()
+        else:
+            print("Thanl you for playing , Goodbye.")
 
-
-              
-          
 # play against computer
 def computer(board):
-     while currentPlayer == "o":
-          position = random.randint(0, 8)
-          if board[position] == "-":
-               board[position] = "o"
-               switchPlayer()
+    while currentPlayer == "o":
+        position = random.randint(0, 8)
+        if board[position] == "-":
+            board[position] = "o"
+            switchPlayer()
 
-"""
-def playAgain():
-          askUser = input("Thank you for playing !!, would you like to play again?, press y for yes, n for no. ")
-          if askUser == "y":
-               board = [ "-", "-", "-",
-                         "-", "-", "-",
-                         "-", "-", "-"]
-               currentPlayer = "x"
-               winner = None
-               gameRunning = True 
-               printBoard()
-               playerInput(board)
-               checkwinner()
-               checkTie(board)
-               switchPlayer()
-               computer(board)
-               checkwinner()
-               checkTie(board)
-          else: 
-               askUser == "n"
-               input("Thank you for playing. goodbye. ")
-"""
-"""
-while gameRunning:
+
+def main():
+    getUsername()
     printBoard()
-    playerInput(board)
+    playerInput()
     checkwinner()
     checkTie(board)
     switchPlayer()
     computer(board)
     checkwinner()
     checkTie(board)
-"""
 
+
+main()
